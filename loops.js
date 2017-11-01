69bd9040d9ceab9a514e1a5632da8a927273068e
Easwari Ramani <eshuprad@gmail.com>
2017-11-01 11:10:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function doWhileLoop(arr){
   //var len=arr.length;
-  while(arr.length >0 && maybeTrue()){
+do {
     arr.pop();
     }
   return arr;
-}
+}while(arr.length >0 && maybeTrue());
 function maybeTrue() {
    return Math.random() >= 0.5;
 }
